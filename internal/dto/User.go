9aac3accf852c5bc887f4abac56466828587ac51
Hiep <vuxuanhiep1709@gmail.com>
2025-06-16 17:39:55 +0700

    blog-api

--- internal/dto/User.go
+++ internal/dto/User.go
@@ -17,3 +17,19 @@ type UserResponse struct {
     Email    string `json:"email"`
     Role     string `json:"role"`
 }
+
+type UserUpdateRequest struct {
+	Username string `json:"username" binding:"omitempty,min=3,max=20"` // update only if valid
+	Email    string `json:"email" binding:"omitempty,email"`           // Validate format if valid
+	Password string `json:"password" binding:"omitempty,min=6"`        // hashed when save before
+}
+
+type AdminUpdateUserRequest struct {
+	UserUpdateRequest
+	Role             string `json:"role" binding:"omitempty,oneof=admin client"` // Only admin can post
+}
+
+type ChangePasswordRequest struct {
+	OldPassword string `json:"old_password" binding:"required"`
+	NewPassword string `json:"new_password" binding:"required,min=6"`
+}
